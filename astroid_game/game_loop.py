51323bd3a180c35e94f933a4c5352b7ba6658cc7
TheAoJo <a2law4life@gmail.com>
2026-09-17 00:36:36 +0300

    removed the code from the main file and moved it to a new file

---
+++ astroid_game/game_loop.py
@@ -0,0 +1,51 @@
+import sys
+
+import pygame
+from asteroid import Asteroid
+from asteroidsfield import AsteroidField
+from shot import Shot
+from player import Player
+from constants import *
+from logger import log_state, log_event
+
+def game_call():
+    print(f"Starting Asteroids")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+    shots = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    AsteroidField.containers = (updatable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    Player.containers = (updatable, drawable)
+    Shot.containers = (shots, updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
+
+    while True:
+        log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        updatable.update(dt)
+        shots.update(dt)
+        for asteroid in asteroids:
+            if player.collides_with(asteroid):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.split()
+                    shot.kill()
+        for draws in drawable:
+            draws.draw(screen)
+        pygame.display.flip()
+        dt = clock.tick(60) / 1000
